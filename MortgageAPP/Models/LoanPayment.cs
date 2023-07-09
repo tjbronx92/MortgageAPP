73e7f2c027e588bfc6811dc4bb0a499e2a38f360
tjbronx92 <39333780+tjbronx92@users.noreply.github.com>
2023-07-08 19:39:15 -0700

    Init

---
+++ MortgageAPP/Models/LoanPayment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace MortgageAPP.Models
+{
+	public class LoanPayment
+	{
+		public int Month { get; set; }
+
+		public decimal Payment { get; set; }
+
+		public decimal MonthlyPrincipal { get; set; }
+
+		public decimal MonthlyInterest { get; set; }
+
+		public decimal TotalInterest { get; set; }
+
+		public decimal Balance { get; set; }
+
+	}
+}
+
